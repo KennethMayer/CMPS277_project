d2521be8f2844df69f18cf376cccbdfb32a499db
KennethMayer <42129683+KennethMayer@users.noreply.github.com>
2019-02-17 13:27:40 -0800

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,7 +1,8 @@
+# database skeleton to test shell and transaction manager layer.
 
 class database:
 	def __init__(self):
-		self.datastore = []
+		self.datastore = {}
 	
 	def read(isbn):
 		return [('krmayer', 18)] # hardcoded for tests
